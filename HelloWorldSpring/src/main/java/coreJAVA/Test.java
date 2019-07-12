10cc15758bf932bcd97d7e070f43aaec184e5769
St-201906141 <St-201906141@LV040.mcit.local>
2019-07-12 19:49:38 -0400

    random 2

--- HelloWorldSpring/src/main/java/coreJAVA/Test.java
+++ HelloWorldSpring/src/main/java/coreJAVA/Test.java
@@ -1,25 +1,15 @@
 package coreJAVA;
 
-public class CoreJava {
+public class Test {
 	int i=0;
 	static int j=0;
 	
 	public static void main(String[] args) {
 	
-		CoreJava c1 = new CoreJava();
-		CoreJava c2 = new CoreJava();
-		
-		c1.i= 2;
-		c2.i=3;
-		System.out.println("c1.i="+c1.i);		
-		System.out.println("c2.i="+c2.i);
-		System.out.println("c1.i="+c1.i);
-		
-		c1.j=4;
-		System.out.println("c1.j="+c1.j);
-		c2.j=5;
-		System.out.println("c2.j="+c2.j);
-		System.out.println("c1.j="+c1.j);
+		String s[] = {"de","va", "ng", "ja", "ys", "wa", "l"};
+		for(String s1: s) {
+			System.out.println(s1);
+		}
 	}
 
 }
--- SpringBareMinimum/src/main/java/beans/Test.java
+++ SpringBareMinimum/src/main/java/beans/Test.java
@@ -1,5 +1,10 @@
 package beans;
 
+import java.util.*;
+
 public class Test {
-	
+	private List fruits;
+	private Set cricketers;
+	// country capitals
+	private Map cc;
 }
