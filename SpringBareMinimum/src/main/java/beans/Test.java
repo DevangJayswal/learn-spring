b3c74bdb9fb9f0dda15f240cb094aee5e9869d4f
St-201906141 <St-201906141@LV040.mcit.local>
2019-07-12 20:28:12 -0400

    random 5

--- SpringBareMinimum/src/main/java/beans/Test.java
+++ SpringBareMinimum/src/main/java/beans/Test.java
@@ -3,8 +3,5 @@ package beans;
 import java.util.*;
 
 public class Test {
-	private List fruits;
-	private Set cricketers;
-	// country capitals
-	private Map cc;
+	
 }
--- SpringBareMinimum/src/main/java/test/Client.java
+++ SpringBareMinimum/src/main/java/test/Client.java
@@ -3,19 +3,15 @@ package test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
+import beans.Test;
 
 public class Client {
 
 	public static void main(String[] args) {
 
-		 
-//		String files[] = new String[] { "resources/car.xml", "resources/engine.xml" };
-//		ApplicationContext ap = new ClassPathXmlApplicationContext(files);
 		ApplicationContext ap = new ClassPathXmlApplicationContext("resources/spring.xml");
-//		Car c = (Car) ap.getBean("c");
-//		c.printCarData();
-		
+//		Test t = (Test) ap.getBean("t");
+//		t.printData();
 
 	}
 
