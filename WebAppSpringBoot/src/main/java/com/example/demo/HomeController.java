83323c5b36f36373ef7f14600057cd713980efa4
devang <devang@devang-laptop>
2019-08-15 17:10:29 -0400

    in spring boot autoconfiguration it will by default search for "jsp" into "/main/webapp/" folder, spring boot by default does not support jsp so we need to add dependency

---
+++ WebAppSpringBoot/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// in spring boot mvc we have embedded tomcat, so
+// do not need to add server explicitly
+
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("home")
+	public String home() {
+		System.out.println("home.jsp");
+		return "home.jsp";
+
+	}
+}
--- WebAppSpringBoot/src/main/java/com/example/demo/WebAppSpringBootApplication.java
+++ WebAppSpringBoot/src/main/java/com/example/demo/WebAppSpringBootApplication.java
@@ -9,5 +9,4 @@ public class WebAppSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebAppSpringBootApplication.class, args);
 	}
-
 }
