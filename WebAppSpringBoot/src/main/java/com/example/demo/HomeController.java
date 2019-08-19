d63147f3f5a99623bf1372cb07986c74abd79e63
DevangJayswal <dnjayswal@gmail.com>
2019-08-19 19:14:21 -0400

    accepting client data using Spring HttpServletRequest
    we do not need request and response both object in spring as compared to
    conventional servlet, it is fine to refer only one object

--- WebAppSpringBoot/src/main/java/com/example/demo/HomeController.java
+++ WebAppSpringBoot/src/main/java/com/example/demo/HomeController.java
@@ -1,18 +1,20 @@
 package com.example.demo;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // in spring boot mvc we have embedded tomcat, so
 // do not need to add server explicitly
 
-
 @Controller
 public class HomeController {
 
 	@RequestMapping("home")
-	public String home() {
-		System.out.println("home.jsp");
+	public String home(HttpServletRequest request) {
+		String name = request.getParameter("nam");
+		System.out.println("hi " + name);
 		return "home";
 	}
 }
