2245bff1bb1fcdccc2901e014b97ed452c9302a4
DevangJayswal <dnjayswal@gmail.com>
2019-08-19 19:56:50 -0400

    using HttpSession

--- WebAppSpringBoot/src/main/java/com/example/demo/HomeController.java
+++ WebAppSpringBoot/src/main/java/com/example/demo/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,9 @@ public class HomeController {
 	@RequestMapping("home")
 	public String home(HttpServletRequest request) {
 		String name = request.getParameter("nam");
+		HttpSession session = request.getSession();
+		
+		session.setAttribute("na", name);
 		System.out.println("hi " + name);
 		return "home";
 	}
--- WebAppSpringBoot/src/main/webapp/pages/home.jsp
+++ WebAppSpringBoot/src/main/webapp/pages/home.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-I love JAVA
+${na} love JAVA
 </body>
 </html>
