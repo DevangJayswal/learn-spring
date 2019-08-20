b9c1aa7a488e42f9ce22068a133fea6792fa5560
DevangJayswal <dnjayswal@gmail.com>
2019-08-19 20:37:49 -0400

    getting rid of HttpSession by replacing it with ModelAndView object
    of Spring framework

--- WebAppSpringBoot/src/main/java/com/example/demo/HomeController.java
+++ WebAppSpringBoot/src/main/java/com/example/demo/HomeController.java
@@ -1,11 +1,9 @@
 package com.example.demo;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 // in spring boot mvc we have embedded tomcat, so
 // do not need to add server explicitly
@@ -14,9 +12,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HomeController {
 
 	@RequestMapping("home")
-	public String home(@RequestParam("nam") String myName, HttpSession session) {		
-		session.setAttribute("na", myName);
-		System.out.println("hi " + myName);
-		return "home";
+	public ModelAndView home(@RequestParam("nam") String myName) {		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("na", myName);
+		mv.setViewName("home");
+		
+		return mv;
 	}
 }
