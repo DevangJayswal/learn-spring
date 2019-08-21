4f5a0f959f082bfa5ddccae657e27bcca1cbaea3
DevangJayswal <dnjayswal@gmail.com>
2019-08-21 17:55:50 -0400

    creating table in H2 database using JPA annotation

--- SpringBootJPA/src/main/java/com/example/demo/model/Alien.java
+++ SpringBootJPA/src/main/java/com/example/demo/model/Alien.java
@@ -1,6 +1,11 @@
 package com.example.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Alien {
+	@Id
 	private int aid;
 	private String aname;
 	
