0f2214597d69f3d3ec1890ad8ee7919de267e03e
DevangJayswal <dnjayswal@gmail.com>
2019-08-21 18:21:18 -0400

    introduction to spring CrudRepository interface
    saving value to the database through html form

--- SpringBootJPA/src/main/java/com/example/demo/controller/AlienController.java
+++ SpringBootJPA/src/main/java/com/example/demo/controller/AlienController.java
@@ -1,13 +1,29 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.dao.AlienRepo;
+import com.example.demo.model.Alien;
+
 @Controller
 public class AlienController {
+	
+	
+	// it will create object of AlienRepo which extends CrudRepository 
+	// then spring will instantiate CrudRepository so that we can make crud operation 
+	@Autowired
+	AlienRepo repo;
 
 	@RequestMapping("/")
 	public String home() {
+		return "home.jsp";
+	}
+	
+	@RequestMapping("/addAlien")
+	public String addAlien(Alien alien) {
+		repo.save(alien);
 		return "home.jsp";
 	}
 }
---
+++ SpringBootJPA/src/main/java/com/example/demo/dao/AlienRepo.java
@@ -0,0 +1,14 @@
+package com.example.demo.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.model.Alien;
+
+// Alien --> responsible to work with Alien
+// Integer --> primary key is type of integer 
+
+
+// we are not implementing AlienRepo anywhere, Spring Data Rest is responsible for that
+public interface AlienRepo extends CrudRepository<Alien, Integer> {
+
+}
