89936d9f4ac157bb0cc1437f9ed412abeede9724
DevangJayswal <dnjayswal@gmail.com>
2019-09-07 13:50:14 -0400

    REST returning value in JSON format, removed toString() method
    "jackson-core.." jar file is responsible for converting java objects into JSON format. That is already there in maven depedency

--- SpringBootJPA/src/main/java/com/example/demo/controller/AlienController.java
+++ SpringBootJPA/src/main/java/com/example/demo/controller/AlienController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -33,31 +34,16 @@ public class AlienController {
 		return "home.jsp";
 	}
 	
-	
-	// when we have return type as String, it means that we are reurning the view
-	// i.e JSP or any other view
-	// but we in REST we are not concerned about view
-	// we are returning a data, so we need to use @ResponseBody
-	// to inform dispatcher servlet that we are not returning a view
-	// we are returning an actual data
 	@RequestMapping("/aliens")
 	@ResponseBody
-	public String getAliens() {
-		
-		// findAll() returns Iterable 
-		// but we have String return type
-		// so we used toString() to convert
-		return repo.findAll().toString();
-		
-		/* output will be List format in String (Not XML or JSON) */
+	public List<Alien> getAliens() {
+		return (List<Alien>) repo.findAll();
 	}
-	
 	
 	@RequestMapping("/alien/{aid}")
 	@ResponseBody
-	public String getAlien(@PathVariable("aid") int aid) {
-		return repo.findById(aid).toString();
+	public Optional<Alien> getAlien(@PathVariable("aid") int aid) {
+		return repo.findById(aid);
 		
 	}
-
 }
