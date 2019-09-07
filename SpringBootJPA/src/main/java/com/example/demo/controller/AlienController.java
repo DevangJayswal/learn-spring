fe0fcf6882a813f5e2cf01a54a8f4e91101bf3f6
DevangJayswal <dnjayswal@gmail.com>
2019-09-07 13:37:58 -0400

    REST findById(any), find aliens with any id. use of @PathVariable

--- SpringBootJPA/src/main/java/com/example/demo/controller/AlienController.java
+++ SpringBootJPA/src/main/java/com/example/demo/controller/AlienController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -52,10 +53,10 @@ public class AlienController {
 	}
 	
 	
-	@RequestMapping("/alien/102")
+	@RequestMapping("/alien/{aid}")
 	@ResponseBody
-	public String getAlien() {
-		return repo.findById(102).toString();
+	public String getAlien(@PathVariable("aid") int aid) {
+		return repo.findById(aid).toString();
 		
 	}
 
